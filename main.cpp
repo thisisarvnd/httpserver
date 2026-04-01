43afd5310c3dd3395bf169369544e22e3ee646e1
Aravind Srinivas <aravindsrinivas.g@gmail.com>
2026-04-01 12:01:00 +0000

    sends a basic response

--- main.cpp
+++ main.cpp
@@ -18,6 +18,13 @@ int main() {
 
     // --- Future steps (Bind, Listen, Accept) go here ---
 
+    // create socket that listen for ipv4 on any interface.
+    // sockaddr is the generic struct that unix bind() returns or expects
+    // you send this sockaddr struct to let the kernel know what and where to listen
+    // sockaddr_in is basically ipv4 struct of sockaddr which is generic (kernel supports a lot of comms)
+    // we cast this ipv4 struct to a generic struct, which is what the kernel expects
+
+
     struct sockaddr_in address;
     address = {0};
     
@@ -25,16 +32,23 @@ int main() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
+    
+
+    //bind the socket
+
     if(bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
         return 1;
     }
 
+    // listen at this socket
     if (listen(server_fd, 10) < 0){
         perror("Listening failed");
         return 1;
     }
 
+    // accept new connections and have a separate socket for each connection
+    // this ensures main socket stays listening for new connections
     int addrlen = sizeof(address);
     int new_connection = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
 
@@ -45,6 +59,26 @@ int main() {
         perror("Error accepting clients");
         return 1;
     }
+
+    // read the header
+    char buffer[1024] = {0};
+    int valread = read(new_connection, buffer, 1024);
+
+    std::cout << "\n\n\n---Request---\n" << buffer << "-----\n" << std::endl;
+
+    // send response
+
+    std::string hello = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello world!";
+
+    int valsend = send(new_connection, hello.c_str(), hello.size(), 0);
+
+    std::cout << "response sent" << valsend << " " << sizeof(hello) << std::endl;
+
+    // I created a bug here. corrected it now
+    // std::string is a class so use its methods to retrieve the value
+
+    // close client connection
+    close(new_connection);
 
     // Clean up before exiting
     close(server_fd);
